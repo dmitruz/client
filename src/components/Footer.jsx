00aaa34ef05190f19eeb959849cfbe2a42979bbe
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-05-07 01:22:50 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
-<footer className="footer">
-    <div className="footerText">
-        <p> "Copyright © "
-            "2026" " by Dmytro Ruzhytskyi"
-            | All Rights Reserved."
-        </p>
-    </div>
-    <div className="footerIconTop">
-        <a href="#home"></a>
-    </div>
-</footer>
+
+const Footer = () => {
+    <footer className="footer">
+        <div className="footerText">
+            <p> "Copyright © "
+                "2026" " by Dmytro Ruzhytskyi"
+                | All Rights Reserved."
+            </p>
+        </div>
+        <div className="footerIconTop">
+            <a href="#home"></a>
+        </div>
+    </footer>
+};
+
+export default Footer;
