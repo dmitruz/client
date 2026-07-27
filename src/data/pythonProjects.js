70d0409b43feb7f0cf1215dafc1ec19e8c8a6b9d
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-07-27 15:15:02 +0300

    add new project

--- src/data/pythonProjects.js
+++ src/data/pythonProjects.js
@@ -1,6 +1,7 @@
 import customersCohort from "../images/customers_cohort.png";
 import natzeroCountries from "../images/nat_zero_countries.png";
 import europeTravelData from "../images/europe_travel.png";
+import retentionImg from "../images/retention.png";
 
 export const pythonProjects = [
   {
@@ -20,6 +21,12 @@ export const pythonProjects = [
     description: "Python + Power BI analysis of europe travel data",
     image: europeTravelData,
     link: "https://github.com/dmitruzik/europe_travel_data"
+  },
+  {
+    title: "Furniture Store Sales",
+    description: "Analysis of furniture sales, using (Pandas, Scikit-learn, machine-learning, matplotlib, Tableau)",
+    image: retentionImg,
+    link: "https://github.com/dmitruzik/furniture_store_sales"
   },
  
 ];
